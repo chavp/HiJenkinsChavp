c38e13dbdb9d1290d65995e11645a7dcef22d8d6
chavp <my.parinya@gmail.com>
2014-02-20 15:16:25 +0700

    init Projects
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ HiJenkins.Module/HiJenkinsSpeaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiJenkins.Module
+{
+    public class HiJenkinsSpeaker
+    {
+        public string Hi(string name)
+        {
+            return string.Format("Hi, {0}. I'm Jenkins.", name);
+        }
+    }
+}
